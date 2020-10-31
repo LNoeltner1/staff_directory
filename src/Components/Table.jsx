ad9227bc7031e1e71a3a291c4cbb9a4479450ed9
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-31 18:56:06 -0400

    added api placeholders in Table.jsx

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -15,26 +15,11 @@ const Table = (props) => {
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>1234322</td>
-                    <td>@aol</td>
-                    <td>December 12</td>
-                    
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>12343431</td>
-                    <td>@gmail</td>
-                    <td>November 4</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>4`37``37</td>
-                    <td>@twitter</td>
-                    <td>April 1</td>
+                    <th scope="row"><img src={props.image} alt="staff-picture" /></th>
+                    <td>{props.first}</td>
+                    <td>{props.last}</td>
+                    <td>{props.email}</td>
+                    <td>{props.age}</td>
                 </tr>
             </tbody>
         </table>
