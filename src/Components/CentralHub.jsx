a30dec2f085e2e52d25a70378c711e1f0d0d1f15
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-31 19:51:32 -0400

    added handleInputChange function

--- src/Components/CentralHub.jsx
+++ src/Components/CentralHub.jsx
@@ -4,7 +4,40 @@ import API from "../utils/API";
 import Header from "./Header"
 import Table from "./Table"
 
+class CentralHub extends React.Component {
+    state = {
+        staff: [],
+        search: "",
+        narrowedStaff: [],
+    };
 
+    componentDidMount() {
+        API.populateTable()
+        .then((res) => {
+            this.setState({
+                staff: res.data.results,
+                narrowedStaff: res.data.results,
+            }); 
+        })
+        .catch ((err) => console.log(err));
+    }
+    handleInputChange = (event) => {
+        this.setState({ search: event.target.value});
+        const searched = this.state.staff.filter((staffMember) => {
+            return (
+                staffMember.name.first
+                .toLowerCase()
+                .indexOf(event.target.value.toLowerCase()) !== -1
+            );
+        });
+        this.setState({ narrowedStaff: searched});
+    };
+
+
+}
+
+export default CentralHub;
+    
 // function Search (props) {
 //     return (
         
