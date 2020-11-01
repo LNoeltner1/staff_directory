9a22228de4173b94d924f4f2d52b527d83c17011
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-31 20:16:19 -0400

    fleshed out fn's in CentralHub and added render area; on to testing and refining

--- src/Components/CentralHub.jsx
+++ src/Components/CentralHub.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+
 import API from "../utils/API";
 import Header from "./Header"
 import Table from "./Table"
@@ -32,6 +32,40 @@ class CentralHub extends React.Component {
         });
         this.setState({ narrowedStaff: searched});
     };
+
+    render () {
+        const staffInfo = this.state.narrowedStaff.map((staffMember, i) => {
+            return (
+                <Table key={i}
+                    first={staffMember.name.first}
+                    last={staffMember.name.last}
+                    image={staffMember.picture.thumbnail}
+                    email={staffMember.email}
+                    age={staffMember.dob.age}                
+                />
+            );
+        });
+
+        return (
+            <>
+                <Header handleInputChange={this.handleInputChange} value={this.state.search}/>
+                
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Age</th>
+                        </tr>
+                    </thead>
+                    {staffInfo}
+                </table>
+
+            </>
+        )
+    }
 
 
 }
--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -10,12 +10,12 @@ const Table = (props) => {
                     <th scope="col">First Name</th>
                     <th scope="col">Last Name</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Winters Old</th>
+                    <th scope="col">Age</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row"><img src={props.image} alt="staff" /></th>
+                    <th scope="row"><img src={props.image} alt="staff-member" /></th>
                     <td>{props.first}</td>
                     <td>{props.last}</td>
                     <td>{props.email}</td>
