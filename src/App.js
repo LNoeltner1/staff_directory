85100dd27d2cb3068046bae958f06f726f978a96
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-31 23:49:23 -0400

    got table to load correctly from API; working on issue with input; still getting some errors

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import Header from "./Components/Header.jsx";
-import Table from "./Components/Table.jsx";
-import React, { Component } from "react";
-import axios from "axios";
+import HeaderTable from "./Components/HeaderTable";
 
-class App extends Component {
-  state = {
-    staff: [],
-    narrowedStaff: [],
-    alphabetical: false,
-    loading: false,
-  };
-
-  componentDidMount() {
-    const res = axios.get("https://randomuser.me/api/?results=20");
-    this.setState({
-      staff: res.data.results,
-      narrowedStaff: res.data.results,
-    });
-  }
-
-  handleInputChange = (event) => {
-    const narrowedStaffMember = event.target.value;
-    const narrowedStaff = this.state.narrowedStaff.filter((staffMember) => {
-      return staffMember.name.last.indexOf(narrowedStaffMember);
-    });
-    this.setState({ narrowedStaff: narrowedStaff });
-  };
-  //make function to handle sorting by last name
-  // handleSort = (event) => {
-  //   event.preventDefault();
-  //   console.log(this.state.search);
-  // };
-
-  render() {
-    return (
-      <>
-        <Header handleInputChange={this.handleInputChange} />
-        <Table narrowedStaff={this.state.narrowedStaff} />
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <HeaderTable />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/HeaderTable.jsx
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+class HeaderTable extends Component {
+  state = {
+    staff: [],
+    narrowedStaff: [],
+  };
+
+  componentDidMount() {
+    this.staffInfo();
+  }
+  staffInfo = function () {
+      axios.get("https://randomuser.me/api/?results=20&nat=us").then((response) => {
+          console.log(response.data.results);
+          this.setState({
+              //both below ended in .results
+              staff: response.data.results,
+              narrowedStaff: response.data.results,
+          });
+      })
+      .catch((err) => {if (err) throw err});
+  }
+
+  handleInputChange = (event) => {
+    event.preventDefault();
+    const userInput = event.target.value;
+    const searchResult = this.state.staff.filter((name) => name.name.last.indexOf(userInput) !== -1);
+    console.log(userInput);
+    this.setState({ narrowedStaff: searchResult });
+  };
+
+
+
+    //   make function to handle sorting by last name
+  handleSort = () => {
+    console.log("clicked 'last'")
+    const sortedStaff = this.state.data.sort((a, b) => {
+        return a.name.last < b.name.last ? -1 : 1;
+    });
+    console.log(sortedStaff);
+    this.setState({
+        narrowedStaff: sortedStaff
+    });
+  };
+
+  render() {
+    return (
+        <>
+            <div className="header">
+                <h1>Staff Directory</h1>
+                <p>Click on carrots to filter by heading or use search box to narrow your results by last name</p>      
+                <input placeholder="Search by Last Name..." onChange={(event) => this.props.handleInputChange(event)} />
+            </div>
+            <div className="container-fluid">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col"><h2>Image</h2></th>
+                            <th scope="col"><h2>First Name</h2></th>
+                            <th scope="col"><h2>Last Name</h2></th>
+                            <th scope="col"><h2>Email</h2></th>
+                            <th scope="col"><h2>Age</h2></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.staff.map((results) => {
+                            // console.log(user);
+                            return (
+                                <tr>
+                                    <th scope="row"><img src={results.picture.thumbnail} className="img-fluid" alt="thumbnail" /></th>
+                                    <td>{results.name.first}</td>
+                                    <td>{results.name.last}</td>
+                                    <td><a href={results.email}>{results.email}</a></td>
+                                    <td>{results.dob.age}</td>
+                                </tr>  
+                            );
+                        })} 
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+  }
+}
+
+export default HeaderTable;
+
+
+// const HeaderTable = ({staffMember: {picture, name, email, dob}}) => {
+//     return (
+//         <>
+//             <div className="header">
+//                 <h1>Staff Directory</h1>
+//                 <p>Click on carrots to filter by heading or use search box to narrow your results by last name</p>      
+//                 <input placeholder="Search by Last Name..." onChange={(event) => this.props.handleInputChange(event)} />
+//             </div>
+//             <table className="table table-striped">
+//                 <thead>
+//                     <tr>
+//                         <th scope="col"><h2>Image</h2></th>
+//                         <th scope="col"><h2>First Name</h2></th>
+//                         <th scope="col"><h2>Last Name</h2></th>
+//                         <th scope="col"><h2>Email</h2></th>
+//                         <th scope="col"><h2>Age</h2></th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <th scope="row"></th>
+//                         <td><img src={picture.thumbnail} className="img-fluid" alt="thumbnail" /></td>
+//                         <td>{name.first}</td>
+//                         <td>{name.last}</td>
+//                         <td><a href={email}>{email}</a></td>
+//                         <td>{dob.age}</td>
+//                     </tr>
+//                 </tbody>
+//             </table>
+//         </>
+//     );
+// }
+
+
+// export default HeaderTable;
